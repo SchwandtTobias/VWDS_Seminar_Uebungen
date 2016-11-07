69987fb78f1f8a58cad4616d37cf3efe2612c711
Tobias Schwandt <tobias.schwandt@tu-ilmenau.de>
2016-11-07 08:59:21 +0100

    Initial unity project

---
+++ Assets/Skripte/ContinousRotation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContinousRotation : MonoBehaviour 
+{
+
+	public float m_AngleInDegreeX = 0.0f;
+	public float m_AngleInDegreeY = 0.0f;
+	public float m_AngleInDegreeZ = 0.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.rotation = Quaternion.Euler (
+			transform.rotation.eulerAngles.x + Mathf.Deg2Rad * m_AngleInDegreeX * Time.deltaTime,
+			transform.rotation.eulerAngles.y + Mathf.Deg2Rad * m_AngleInDegreeY * Time.deltaTime,
+			transform.rotation.eulerAngles.z + Mathf.Deg2Rad * m_AngleInDegreeZ * Time.deltaTime);
+	}
+}
