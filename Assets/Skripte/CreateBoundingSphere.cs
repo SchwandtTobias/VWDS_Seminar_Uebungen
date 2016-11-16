4c96791a753fe75e51fb457b6518d9f5b6962f2e
Tobias Schwandt <t.schwandt@gmail.com>
2016-11-16 12:37:09 +0100

    Add new scene and script for collision detection

---
+++ Assets/Skripte/CreateBoundingSphere.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateBoundingSphere : MonoBehaviour
+{
+
+    private Vector3 Mittelpunkt;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Mesh Modell = GetComponent<MeshFilter>().mesh;
+        Vector3[] Vertices = Modell.vertices;
+
+        // -----------------------------------------------------------------------------
+        // Mittelpunkt
+        // -----------------------------------------------------------------------------
+        Vector3 Mittelpunkt = new Vector3(0, 0, 0);
+
+        for (int IndexOfVertex = 0; IndexOfVertex < Vertices.Length; ++IndexOfVertex)
+        {
+            Mittelpunkt += Vertices[IndexOfVertex];
+        }
+
+        Mittelpunkt = Mittelpunkt / Vertices.Length;
+
+        // -----------------------------------------------------------------------------
+        // Radius
+        // -----------------------------------------------------------------------------
+        float Radius = 0.0f;
+
+        for (int IndexOfVertex = 0; IndexOfVertex < Vertices.Length; ++IndexOfVertex)
+        {
+            float Distance = Vector3.Distance(Mittelpunkt, Vertices[IndexOfVertex]);
+
+            if (Distance > Radius)
+            {
+                Radius = Distance;
+            }
+        }
+
+        Debug.Log(Mittelpunkt);
+        Debug.Log(Radius);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+    }
+}
