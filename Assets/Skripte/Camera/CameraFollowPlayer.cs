51cdf7872aa57c60870be2c27ae0aeaf68272e83
Tobias Schwandt <t.schwandt@gmail.com>
2016-11-28 12:25:54 +0100

    Add interation example

---
+++ Assets/Skripte/Camera/CameraFollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform m_FollowObject;
+
+	void Start ()
+    {
+	
+	}
+
+	void Update ()
+    {
+        if (m_FollowObject != null)
+        {
+            transform.position = new Vector3(m_FollowObject.position.x, m_FollowObject.position.y, transform.position.z);
+        }
+	}
+}
---
+++ Assets/Skripte/Obstacles/FireBlock.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBlock : GeneralBlock
+{
+    
+	void Start ()
+    {
+	
+	}
+
+	void Update ()
+    {
+	
+	}
+}
