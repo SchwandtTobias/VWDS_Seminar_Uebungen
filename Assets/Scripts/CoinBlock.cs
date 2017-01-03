3486e59b0fa464c4634e719052b5653747ecc95c
Tobias Schwandt <t.schwandt@gmail.com>
2017-01-03 09:24:53 +0100

    Add Robo example

---
+++ Assets/Scripts/CoinBlock.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinBlock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D _Other)
+	{
+		GameObject OtherGO = _Other.gameObject;
+
+		if (OtherGO.CompareTag ("Player")) 
+		{
+			RoboAbbilities Abbilities = OtherGO.GetComponent<RoboAbbilities> ();
+
+			if (Abbilities != null) {
+				Abbilities.m_Coins += 1;
+
+				Destroy (gameObject);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UI : MonoBehaviour {
+
+	public RoboAbbilities m_Abbilities;
+
+	public Text m_Coins;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		m_Coins.text = "Coins: " + m_Abbilities.m_Coins;
+	}
+}
