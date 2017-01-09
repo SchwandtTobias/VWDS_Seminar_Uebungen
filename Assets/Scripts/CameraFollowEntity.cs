a5697d687a690c45f6f0ff48e7163ceec3f0f325
Tobias Schwandt <t.schwandt@gmail.com>
2017-01-09 13:23:51 +0100

    Add skript that camera follows player

---
+++ Assets/Scripts/CameraFollowEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowEntity : MonoBehaviour {
+
+    public Transform m_ObjectToFollow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (m_ObjectToFollow != null)
+		{
+            transform.position = new Vector3(m_ObjectToFollow.position.x, m_ObjectToFollow.position.y, transform.position.z);
+		}
+	}
+}
